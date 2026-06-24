10f116a3c46849ed25acc54a7cfa9bde651eaa85
nmoradiya <nitinmoradiya81@gmail.com>
2026-06-24 17:58:32 +0000

    Commit

--- 01 PySpark/01 Read Sources/Text Files.py
+++ 01 PySpark/01 Read Sources/Text Files.py
@@ -1,27 +1,31 @@
 # Databricks notebook source
-df=spark.read.text("/Volumes/training/my_volume/my_volume/log_files/")
+# /// script
+# [tool.databricks.environment]
+# environment_version = "2"
+# ///
+df=spark.read.text("/Volumes/dblearn_nk/raw_datafiles/sourcefiles/log_files/")
 display(df)
 
 # COMMAND ----------
 
-df=spark.read.text("/Volumes/training/my_volume/my_volume/log_files/",wholetext=True)
+df=spark.read.text("/Volumes/dblearn_nk/raw_datafiles/sourcefiles/log_files/",wholetext=True)
 display(df)
 
 # COMMAND ----------
 
-df = spark.read.format("binaryFile").load("/Volumes/training/my_volume/my_volume/log_files/")
+df = spark.read.format("binaryFile").load("/Volumes/dblearn_nk/raw_datafiles/sourcefiles/log_files/")
 display(df)
 
 # COMMAND ----------
 
-df=spark.read.text("/Volumes/training/my_volume/my_volume/log_files/",wholetext=True)
+df=spark.read.text("/Volumes/dblearn_nk/raw_datafiles/sourcefiles/log_files/",wholetext=True)
 df2=df.selectExpr("value", "_metadata.file_path", "_metadata.file_name", "_metadata.file_size", "_metadata.file_modification_time")
 display(df2)
 
 
 # COMMAND ----------
 
-df=spark.read.parquet("/Volumes/training/my_volume/my_volume/parquet_files_2/")
+df=spark.read.parquet("/Volumes/dblearn_nk/raw_datafiles/output_files/parquet_files_sample/")
 display(df)
 
 # COMMAND ----------
