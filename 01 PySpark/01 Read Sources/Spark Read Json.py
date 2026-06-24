3a28023fa82b9ef83cae0a36c09b5f2d60001ca0
nmoradiya <nitinmoradiya81@gmail.com>
2026-06-24 12:51:33 +0000

    Commit - Json Learning

--- 01 PySpark/01 Read Sources/Spark Read Json.py
+++ 01 PySpark/01 Read Sources/Spark Read Json.py
@@ -1,12 +1,13 @@
 # Databricks notebook source
-df=spark.read.json("/Volumes/training/my_volume/my_volume/json_files/sales_single_line.json")
+df=spark.read.json("/Volumes/dblearn_nk/raw_datafiles/sourcefiles/json_files/sales_single_line.json")
 display(df)
 
 # COMMAND ----------
 
-df=spark.read.option("multiline","true").json("/Volumes/training/my_volume/my_volume/json_files/sales_multi_line.json")
+df=spark.read.option("multiline","true").json("/Volumes/dblearn_nk/raw_datafiles/sourcefiles/json_files/sales_multi_line.json")
 display(df)
 
 # COMMAND ----------
 
-df.write.format("csv").save("/Volumes/training/my_volume/my_volume/csv_out_2")
+df.write.format("csv").save("/Volumes/dblearn_nk/raw_datafiles/sourcefiles/csv_out_files/")
+df.write.format("json").save("/Volumes/dblearn_nk/raw_datafiles/sourcefiles/json_out_files/")
